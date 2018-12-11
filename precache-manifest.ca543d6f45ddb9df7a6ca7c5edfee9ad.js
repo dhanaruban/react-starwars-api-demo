1639e1593a6f7e690dcd323b273c55e037c4c478
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-12-11 12:12:07 -0700

    Updates

---
+++ precache-manifest.ca543d6f45ddb9df7a6ca7c5edfee9ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e709b3843fdfa799d06d",
+    "url": "/react-starwars-api-demo/static/js/runtime~main.e709b384.js"
+  },
+  {
+    "revision": "fe33ae05e5989a563d3f",
+    "url": "/react-starwars-api-demo/static/js/main.fe33ae05.chunk.js"
+  },
+  {
+    "revision": "7e5e9edb903621e6f178",
+    "url": "/react-starwars-api-demo/static/js/1.7e5e9edb.chunk.js"
+  },
+  {
+    "revision": "fe33ae05e5989a563d3f",
+    "url": "/react-starwars-api-demo/static/css/main.271595d8.chunk.css"
+  },
+  {
+    "revision": "f03044b4479d0fdca8cfe73be8a87914",
+    "url": "/react-starwars-api-demo/index.html"
+  }
+];
