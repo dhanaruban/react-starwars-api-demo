973f080e94a04cfecf65c09f6bd439b94409771d
Joseph Podwys <jpodwys@josephpodwysmbp.lan>
2018-12-11 11:03:36 -0700

    Indentation

--- src/action-creators.js
+++ src/action-creators.js
@@ -1,28 +1,28 @@
 import { getPeople, getPlanets, getStarships } from './sw-service';
 
 export function GET_PEOPLE() {
-	return dispatch => {
-		dispatch({ type: 'SET_LOADING', value: true });
-		return getPeople().then(res => {
-			dispatch({ type: 'SET_LIST', value: res.results });
-		});
-	}
+  return dispatch => {
+    dispatch({ type: 'SET_LOADING', value: true });
+    return getPeople().then(res => {
+      dispatch({ type: 'SET_LIST', value: res.results });
+    });
+  }
 }
 
 export function GET_PLANETS() {
-	return dispatch => {
-		dispatch({ type: 'SET_LOADING', value: true });
-		return getPlanets().then(res => {
-			dispatch({ type: 'SET_LIST', value: res.results });
-		});
-	}
+  return dispatch => {
+    dispatch({ type: 'SET_LOADING', value: true });
+    return getPlanets().then(res => {
+      dispatch({ type: 'SET_LIST', value: res.results });
+    });
+  }
 }
 			
 export function GET_STARSHIPS() {
-	return dispatch => {
-		dispatch({ type: 'SET_LOADING', value: true });
-		return getStarships().then(res => {
-			dispatch({ type: 'SET_LIST', value: res.results });
-		});
-	}
+  return dispatch => {
+    dispatch({ type: 'SET_LOADING', value: true });
+    return getStarships().then(res => {
+      dispatch({ type: 'SET_LIST', value: res.results });
+    });
+  }
 }
